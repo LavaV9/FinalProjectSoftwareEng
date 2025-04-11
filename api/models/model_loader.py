c6c0f20a9db83fcc18dc286e5ca205120918cd58
Pranav Rao <raopranav@gmail.com>
2025-04-11 14:11:18 -0400

    Got rid of recipes from model_loader (irrelevant)

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources, customers, payments, promotions
+from . import orders, order_details, sandwiches, resources, customers, payments, promotions
 
 from ..dependencies.database import engine
 
@@ -6,7 +6,7 @@ from ..dependencies.database import engine
 def index():
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
+    # recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
     customers.Base.metadata.create_all(engine)
