b87698b2bde488ea1b3c399ba8f9a35c2aa9ed91
Pranav Rao <raopranav@gmail.com>
2025-04-13 11:57:47 -0400

    fixed sandwich related errors/rewrote relationships

--- api/models/sandwiches.py
+++ api/models/sandwiches.py
@@ -1,18 +1,15 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL
 from sqlalchemy.orm import relationship
-from datetime import datetime
 from ..dependencies.database import Base
-
 
 class Sandwich(Base):
     __tablename__ = "sandwiches"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
-    resource_id = Column(Integer, ForeignKey("resources.id"))
     sandwich_name = Column(String(100), unique=True, nullable=True)
     price = Column(DECIMAL(4, 2), nullable=False, server_default='0.0')
 
-    ingredients = relationship("Ingredient", backref="menu")
     menu = relationship("Menu", back_populates="sandwich")
     resources = relationship("Resource", back_populates="sandwich")
+    order_details = relationship("OrderDetail", back_populates="sandwich")
+
--- api/schemas/sandwiches.py
+++ api/schemas/sandwiches.py
@@ -6,23 +6,18 @@ from pydantic import BaseModel
 class SandwichBase(BaseModel):
     sandwich_name: str
     price: float
-    ingredients: Optional[list] = None
 
 
 class SandwichCreate(SandwichBase):
-    sandwich_id: int
     resource_id: int
 
 
 class SandwichUpdate(BaseModel):
     sandwich_name: Optional[str] = None
     price: Optional[float] = None
-    ingredients: Optional[list] = None
 
 
 class Sandwich(SandwichBase):
     id: int
-
-    ingredients: Optional[list] = None
     class ConfigDict:
         from_attributes = True
